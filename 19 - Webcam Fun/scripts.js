41d2ad29fc986fc6b691d4bc5a3951bfa2297a57
minheekangg <minheekangg@gmail.com>
2020-06-24 12:50:27 -0400

    get video done

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,16 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+//get video live stream to render on the screen
+function getVideo () {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(localMediaStream => {
+        console.log(localMediaStream, video.src)
+        video.srcObject = localMediaStream; //live video feed
+        video.play();
+    })
+    .catch(err=>console.log(err))
+}
+
+getVideo();
